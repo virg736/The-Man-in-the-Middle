386369e9facda688b849c4d2d22df2a8cb0880be
Virginie <virginielechene@proton.me>
2025-05-27 01:52:22 +0200

    Update audit-proxy.bat

--- tools/audit-proxy.bat
+++ tools/audit-proxy.bat
@@ -25,3 +25,15 @@ start certmgr.msc
 pause
 
 Add audit-proxy.bat in tools/
+
+
+##  Avertissement légal
+
+> Ce projet est exclusivement destiné à des fins éducatives, dans le cadre de la formation à la cybersécurité.
+> L’auteure ne cautionne ni n’autorise l’utilisation de ces techniques en dehors d’un cadre légal strictement défini.
+> Toute utilisation non autorisée est interdite et relève de la seule responsabilité de l’utilisateur.
+
+##  Auteur / Droits
+
+© 2025 Virginie Lechene — Tous droits réservés.
+Reproduction interdite sans autorisation.
